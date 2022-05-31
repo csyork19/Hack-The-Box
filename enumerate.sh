3f83900cbfe8c753b474afa03d55890de164133e
Christopher <csyork19@gmail.com>
2022-05-30 20:17:51 -0400

    Update enumerate script...not running properly yet

--- enumerate.sh
+++ enumerate.sh
@@ -10,6 +10,15 @@ ip_address=$1
 output_file=$2
 
 ## Perform nmap scan
-echo "Performing NMAP scan on $ip_address"
-nmap -sV -sC -oN $output_file-nmap.txt $ip_address
+printf "\n------ NMAP ------\n\n"
+nmap -sV -sC -A $ip_address >> $output_file-nmap-results.txt
+
+while read line
+do
+    if [[ $line == *open*]] && [[ $line == *http*]]
+    then
+    echo "Running dirb"
+    echo "I will run dirb at a later time when I add it in...."
+    fi
+done < results
 echo "NMAP scan is complete."
